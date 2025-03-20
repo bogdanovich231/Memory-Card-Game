30fdd27bf966ecc0ef5b4bcf481dd9f446c72f66
Tatsiana <kulinkovich56@gmail.com>
2025-03-20 17:44:37 +0100

    refactor(style): card level restructuring

--- src/Components/CardLevel/CardLevel.tsx
+++ src/Components/CardLevel/CardLevel.tsx
@@ -3,14 +3,12 @@ import "./CardLevel.scss";
 const CardLevel = () => {
   return (
     <div className="level-card">
-      <div className="level-card_number">
-        <h2>1</h2>
-      </div>
       <div className="level-card_content">
-        <div className="level-card_content-title">
-          <h4>Light</h4>
+        <div className="level-card_number">
+          <h2>1</h2>
         </div>
         <div className="level-card_content-info">
+          <h4>Light</h4>
           <p>Cards: 8</p>
         </div>
       </div>
